d2093563c58635cae6c94218df1624fd64c9a303
Zack Huang <zackhuang0513@gmail.com>
2023-05-31 16:46:07 +0800

    1. Add PrintHandleInfo func. 2. Fix receive function bug.

--- CircusUart.cpp
+++ CircusUart.cpp
@@ -12,199 +12,201 @@
 
 #include "CircusUart.h"
 
-CircusUart::CircusUart(Stream &stream) : stream(stream), handler_idx(0)
-{
-}
+CircusUart::CircusUart(Stream &stream) : stream(stream), handler_idx(0) {}
 
-CircusUart::~CircusUart()
-{
-}
+CircusUart::~CircusUart() {}
 
-uint8_t CircusUart::receive(void)
-{
-    static char       buffer[CIRCUS_RX_BUFF];
-    static handler_t *handler = NULL;
-    static int        len = 0, cr = false, state = RX_HEADER, ii = 0;
-    static char       type = '\0';
-    char              c;
+uint8_t CircusUart::receive(void) {
+  static char buffer[CIRCUS_RX_BUFF];
+  static handler_t *handler = NULL;
+  static int len = 0, cr = false, state = RX_HEADER, ii = 0;
+  static char type = '\0';
+  char c;
 
-    if (this->stream.available()) {
-        switch (state) {
-        default:
-        case RX_HEADER:
-            /* get header CR*/
-            c = this->stream.read();
+  if (this->stream.available()) {
+    switch (state) {
+    default:
+    case RX_HEADER:
+      /* get header CR*/
+      c = this->stream.read();
 #if READ_DEBUG
-            Serial.print(c);
+      Serial.print(c);
 #endif
-            if (c == '\r') {
-                cr = true;
-            } else if (c == '\n') {
-                if (cr) {
-                    cr    = false;
-                    state = RX_CONTENT;
-                } else {
-                    /* error */
-                    state = RX_ERROR;
-                }
-            } else {
-                state = RX_ERROR;
-            }
-            break;
-        case RX_CONTENT:
-            /* get content */
-            buffer[len] = this->stream.read();
-#if READ_DEBUG
-            Serial.print(buffer[len]);
-#endif
-            if (buffer[len] == '\r') {
-                type        = '\0';
-                cr          = true;
-                buffer[len] = '\0';
-                state       = RX_FOOTER;
-            } else {
-                for (ii = 0; ii < this->handler_idx; ii++) {
-                    if (buffer[len] == this->handlers[ii].separator) {
-                        type        = this->handlers[ii].separator;
-                        buffer[len] = '\0';
-                        if (strcmp(this->handlers[ii].cmd, buffer) == 0) {
-                            /* got it */
-                            handler = (this->handlers + ii);
-                            state   = RX_DATA;
-                            break;
-                        }
-                    }
-                }
-                if (type != '\0' && handler == NULL) {
-                    // Found separator but no find cmd
-                    state = RX_ERROR;
-                } else if (type == '\0' && handler == NULL) {
-                    // no find separator and cmd
-                    // normal character
-                    len++;
-                } else if (type != '\0' && handler != NULL) {
-                    // found separator and cmd
-                    len = 0;
-                }
-            }
-            break;
-        case RX_DATA:
-            this->rec_buff[len] = this->stream.read();
-#if READ_DEBUG
-            Serial.print(this->rec_buff[len]);
-#endif
-            if (this->rec_buff[len] == '\r') {
-                cr                  = true;
-                this->rec_buff[len] = '\0';
-                state               = RX_FOOTER;
-            } else if (this->rec_buff[len] == '\n') {
-                this->rec_buff[len] = '\0';
-                state               = RX_ERROR;
-            } else {
-                // normal character
-                len++;
-            }
-            break;
-        case RX_FOOTER:
-            /* get footer CR/LF */
-            c = this->stream.read();
-#if READ_DEBUG
-            Serial.print(c);
-#endif
-            if (c == '\n') {
-                if (cr) {
-                    cr    = false;
-                    len   = 0;
-                    state = RX_HEADER;
-                    // call callback
-                    for (ii = 0; ii < this->handler_idx; ii++) {
-                        if (type == this->handlers[ii].separator) {
-                            if (strcmp(this->handlers[ii].cmd, buffer) == 0) {
-                                /* got it */
-                                (this->handlers + ii)->func(this->rec_buff);
-                                this->rec_buff[0] = '\0';
-                                break;
-                            }
-                        }
-                    }
-                    if (type != '\0' && handler == NULL) {
-                        // Found separator but no find cmd
-                        state = RX_ERROR;
-                    }
-                    type    = '\0';
-                    handler = NULL;
-                }
-            } else {
-                /* error */
-                state = RX_ERROR;
-            }
-            break;
-        case RX_ERROR:
-            /* error state, flush all data */
-            c = this->stream.read();     // dummy read
-#if READ_DEBUG
-            Serial.print(c);
-#endif
-            if (c == '\n') {
-                cr                = false;
-                len               = 0;
-                state             = RX_HEADER;
-                handler           = NULL;
-                type              = '\0';
-                this->rec_buff[0] = '\0';
-            }
-            break;
+      if (c == '\r') {
+        cr = true;
+      } else if (c == '\n') {
+        if (cr) {
+          cr = false;
+          state = RX_CONTENT;
+        } else {
+          /* error */
+          state = RX_ERROR;
         }
+      } else {
+        state = RX_ERROR;
+      }
+      break;
+    case RX_CONTENT:
+      /* get content */
+      buffer[len] = this->stream.read();
 #if READ_DEBUG
-        Serial.print(state);
+      Serial.print(buffer[len]);
 #endif
-    }
+      if (buffer[len] == '\r') {
+        type = '\0';
+        cr = true;
+        buffer[len] = '\0';
+        state = RX_FOOTER;
+      } else {
+        for (ii = 0; ii < this->handler_idx; ii++) {
+          if (buffer[len] == this->handlers[ii].separator) {
+            type = this->handlers[ii].separator;
+            if (strncmp(this->handlers[ii].cmd, buffer,
+                        strlen(this->handlers[ii].cmd)) == 0) {
+              /* got it */
+              handler = (this->handlers + ii);
+              state = RX_DATA;
+              break;
+            }
+          }
+        }
+        if (type != '\0' && handler == NULL) {
+          // Found separator but no find cmd
+          state = RX_ERROR;
+        } else if (type == '\0' && handler == NULL) {
+          // no find separator and cmd
+          // normal character
+          len++;
+        } else if (type != '\0' && handler != NULL) {
+          // found separator and cmd
+          len = 0;
+        }
+      }
+      break;
+    case RX_DATA:
+      this->rec_buff[len] = this->stream.read();
+#if READ_DEBUG
+      Serial.print(this->rec_buff[len]);
+#endif
+      if (this->rec_buff[len] == '\r') {
+        cr = true;
+        this->rec_buff[len] = '\0';
+        state = RX_FOOTER;
+      } else if (this->rec_buff[len] == '\n') {
+        this->rec_buff[len] = '\0';
+        state = RX_ERROR;
+      } else {
+        // normal character
+        len++;
+      }
+      break;
+    case RX_FOOTER:
+      /* get footer CR/LF */
+      c = this->stream.read();
+#if READ_DEBUG
+      Serial.print(c);
+#endif
+      if (c == '\n') {
+        if (cr) {
+          cr = false;
+          len = 0;
+          state = RX_HEADER;
+          // call callback
+          for (ii = 0; ii < this->handler_idx; ii++) {
+            if (type == this->handlers[ii].separator) {
 
-    if (len) {
-        return len;
-    } else {
-        return ERROR_UART_NO_DATA;
+              if (strncmp(this->handlers[ii].cmd, buffer,
+                          strlen(this->handlers[ii].cmd)) == 0) {
+                /* got it */
+                (this->handlers + ii)->func(this->rec_buff);
+                this->rec_buff[0] = '\0';
+                break;
+              }
+            }
+          }
+          if (type != '\0' && handler == NULL) {
+            // Found separator but no find cmd
+            state = RX_ERROR;
+          }
+          type = '\0';
+          handler = NULL;
+        }
+      } else {
+        /* error */
+        state = RX_ERROR;
+      }
+      break;
+    case RX_ERROR:
+      /* error state, flush all data */
+      c = this->stream.read(); // dummy read
+#if READ_DEBUG
+      Serial.print(c);
+#endif
+      if (c == '\n') {
+        cr = false;
+        len = 0;
+        state = RX_HEADER;
+        handler = NULL;
+        type = '\0';
+        this->rec_buff[0] = '\0';
+      }
+      break;
     }
+#if READ_DEBUG
+    Serial.print(state);
+#endif
+  }
+
+  if (len) {
+    return len;
+  } else {
+    return ERROR_UART_NO_DATA;
+  }
 }
 
-void CircusUart::loop()
-{
-    this->receive();
-}
+void CircusUart::loop() { this->receive(); }
 
-uint16_t CircusUart::send(const char *cmd, const char separator, const char *param)
-{
-    uint16_t writeLen = 0;
+uint16_t CircusUart::send(const char *cmd, const char separator,
+                          const char *param) {
+  uint16_t writeLen = 0;
+  writeLen += this->stream.println();
+  writeLen += this->stream.write(cmd, strlen(cmd));
+  if (separator != '\0' && param != NULL) {
+    writeLen += this->stream.write(separator);
+    writeLen += this->stream.println(param);
+  } else {
     writeLen += this->stream.println();
-    writeLen += this->stream.write(cmd, strlen(cmd));
-    if (separator != '\0' && param != NULL) {
-        writeLen += this->stream.write(separator);
-        writeLen += this->stream.println(param);
-    } else {
-        writeLen += this->stream.println();
-    }
-    return writeLen;
+  }
+  return writeLen;
 }
 
-void CircusUart::on(const char *cmd, const char separator, Func func)
-{
-    if (this->handler_idx < CIRCUS_HANDLER_MAX) {
-        handler_t newHandler;
-        newHandler.cmd                      = (char *)cmd;
-        newHandler.separator                = separator;
-        newHandler.func                     = func;
-        this->handlers[this->handler_idx++] = newHandler;
-    } else {
-        // exceeds the maximum value
-    }
+void CircusUart::on(const char *cmd, const char separator, Func func) {
+  if (this->handler_idx < CIRCUS_HANDLER_MAX) {
+    handler_t newHandler;
+    newHandler.cmd = (char *)cmd;
+    newHandler.separator = separator;
+    newHandler.func = func;
+    this->handlers[this->handler_idx++] = newHandler;
+  } else {
+    // exceeds the maximum value
+  }
 }
 
-size_t CircusUart::write(uint8_t data)
-{
-    return this->stream.write(data);
+size_t CircusUart::write(uint8_t data) { return this->stream.write(data); }
+
+size_t CircusUart::write(const char *buffer, size_t size) {
+  return this->stream.write(buffer, size);
 }
 
-size_t CircusUart::write(const char *buffer, size_t size)
-{
-    return this->stream.write(buffer, size);
+void CircusUart::printHandlesInfo(void) {
+  for (int i = 0; i < this->handler_idx; i++) {
+    Serial.println();
+    Serial.print("Index => ");
+    Serial.println(i);
+    Serial.print("Cmd => ");
+    Serial.println(this->handlers[i].cmd);
+    Serial.print("separator => ");
+    Serial.println(this->handlers[i].separator);
+    Serial.println();
+  }
 }
--- CircusUart.h
+++ CircusUart.h
@@ -20,44 +20,44 @@
 
 typedef void (*Func)(const char *data);
 
-typedef enum
-{
-    RX_HEADER,
-    RX_CONTENT,
-    RX_DATA,
-    RX_FOOTER,
-    RX_ERROR,
-    RX_MAX
+typedef enum {
+  RX_HEADER,
+  RX_CONTENT,
+  RX_DATA,
+  RX_FOOTER,
+  RX_ERROR,
+  RX_MAX
 } RX_STATE_E;
 
-typedef enum
-{
-    ERROR_UART_NONE,
-    ERROR_UART_NO_DATA,
+typedef enum {
+  ERROR_UART_NONE,
+  ERROR_UART_NO_DATA,
 } ERROR_CODE_E;
 
 typedef struct {
-    char *cmd;
-    char  separator;
-    Func  func;
+  char *cmd;
+  char separator;
+  Func func;
 } handler_t;
 
 class CircusUart : public Print {
-  private:
-    Stream   &stream;
-    uint8_t   receive(void);
-    handler_t handlers[CIRCUS_HANDLER_MAX];
-    uint8_t   handler_idx;
-    char      rec_buff[CIRCUS_RX_BUFF];
+private:
+  Stream &stream;
+  uint8_t receive(void);
+  handler_t handlers[CIRCUS_HANDLER_MAX];
+  uint8_t handler_idx;
+  char rec_buff[CIRCUS_RX_BUFF];
 
-  public:
-    CircusUart(Stream &stream);
-    ~CircusUart();
-    void     loop();
-    uint16_t send(const char *cmd, const char separator = '\0', const char *param = NULL);
-    void     on(const char *cmd, const char separator, Func func);
-    size_t   write(uint8_t);
-    size_t   write(const char *buffer, size_t size);
+public:
+  CircusUart(Stream &stream);
+  ~CircusUart();
+  void loop();
+  uint16_t send(const char *cmd, const char separator = '\0',
+                const char *param = NULL);
+  void on(const char *cmd, const char separator, Func func);
+  size_t write(uint8_t);
+  size_t write(const char *buffer, size_t size);
+  void printHandlesInfo(void);
 };
 
 #endif
